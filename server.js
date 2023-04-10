a93a54e307e86817f2c563814779d2271dc15856
TheMarvello <dipukumars7476@gmail.com>
2023-04-10 11:10:43 +0530

    Product API backend done

--- server.js
+++ server.js
@@ -4,19 +4,20 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
-import categoryRoutes from "./routes/categoryRoutes.js"
-import cors from 'cors';
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
 
 //configure env
-dotenv.config(); //if dotenv file is in another folder we would define path inside the parenthesis
+dotenv.config();
 
-//database config
+//databse config
 connectDB();
 
 //rest object
 const app = express();
 
-//middlewares
+//middelwares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
@@ -24,10 +25,11 @@ app.use(morgan("dev"));
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 
 //rest api
 app.get("/", (req, res) => {
-    res.send("<h1> Welcome to ecommerce app </h1>");
+    res.send("<h1>Welcome to ecommerce app</h1>");
 });
 
 //PORT
